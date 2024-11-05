f3c3426cd047981c962deaf03c6d33070b02670d
a.stogniy <a.stogniy@astondevs.ru>
2024-11-05 09:11:04 +0300

    initial commit-2

---
+++ src/main/java/com/github/mytelegrambot/mytelegrambot/MytelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.mytelegrambot.mytelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytelegrambotApplication.class, args);
+	}
+
+}
